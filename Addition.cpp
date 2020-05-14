6b85f25e22075c30ec5fbb96abefc27652ed94b6
siddhantPanchal <55625388+siddhantPanchal@users.noreply.github.com>
2020-05-14 11:11:33 +0530

    Addition of 2 numbers

---
+++ Addition.cpp
@@ -0,0 +1,14 @@
+#include<iostream.h>
+#include<conio.h>
+
+void main()
+{
+	int no1 = 0,no2 = 0,result = 0;
+	cout<<"\nEnter No 1 : ";
+	cin>>no1;
+	cout<<"\nEnter No 2 : ";
+	cin>>no2;
+	result = no1+no2;
+	cout<<"\nResult : "<<result;
+getch();
+}
